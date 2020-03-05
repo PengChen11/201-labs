44195629b24cf5d179563394af4176bf36697899
Peng Chen <pchen0816@outlook.com>
2020-03-04 21:44:33 -0800

    questions

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 /* eslint-disable indent */
 'use strict';
+var score = 0;
 
 function greetings(){
     var today = new Date();
@@ -25,113 +26,238 @@ function yourName(){
     while (yourName == '') {
         yourName = prompt('What is your name?');
     }
-    inputName = yourName + '!';
+    inputName = 'Dear ' + yourName + '! ';
     document.getElementById("yourname").innerHTML = inputName ;
 }
 
 
-function quiz(){
-// this is question 1    
-    var q1 = prompt('Have you ever been to China?').toLowerCase();
+function question1(){
+        // alert('You are about to play the guessing game! The first 5 questions will only take "yes", "y", "no" or "n" as the answer. ');
+    // this is question 1    
+    var q1 = prompt('Does the US has more land area than China?').toLowerCase();
     var a1 ;
     while (q1 !=='y' && q1 !== 'yes' && q1 !== 'n' && q1 !== 'no'){
-        q1 = prompt ('Have you ever been to China?').toLowerCase();
+        q1 = prompt ('Invalid input. Does U.S. has more land than China? ').toLowerCase();
     }
     if (q1 == 'y' || q1 == 'yes'){
-        a1 = 'China changes every day, please visit it again once you got a chance.';
+        a1 = 'Acturarely, China is 2.2% larger than the US land area.';
         alert (a1);
-    }    else if (q1 == 'no' || q1 == 'n'){
-        a1 = "Please visit China when you got a chance, you won't regred it!";
+    } else if (q1 == 'no' || q1 == 'n'){
+        a1 = 'Correct! China is 2.2% larger than the US land area.';
         alert (a1);
+        score += 1;
     }
-// this is the question 2
+}
 
-    var q2 = prompt ('Have you ever served in the military?').toLowerCase();
+function question2(){
+    var q2 = prompt ('Will you stop buying corona beer due to the coronavirus?').toLowerCase();
     var a2 ;
     while (q2 !=='y' && q2 !== 'yes' && q2 !== 'n' && q2 !== 'no'){
-        q2 = prompt ('Have you ever served in the military?').toLowerCase();
+        q2 = prompt ('Invalid input. Will you stop buying corona beer due to the coronavirus?').toLowerCase();
     }
     if (q2 == 'y' || q2 == 'yes'){
-        a2 = 'Great! Thank you for your service!';
+        a2 = 'Well, The beer has nothing to do with the virus! Those two are not related at all!';
         alert (a2);
-    }    else if (q2 == 'no' || q2 == 'n'){
-        a2 = 'Military experience is amazing!';
+    } else if (q2 == 'no' || q2 == 'n'){
+        a2 = 'Great! The beer has nothing to do with the virus!';
         alert (a2);
+        score += 1;
     }
+}
 
-// this is question 3
-
-    var q3 = prompt ('Have you ever fixed a helicopter before?').toLowerCase();
+function question3(){
+    var q3 = prompt ('Do you think Apache attach helicopter an only fly 200 miles without refueling?').toLowerCase();
     var a3 ;
     while (q3 !=='y' && q3 !== 'yes' && q3 !== 'n' && q3 !== 'no'){
-        q3 = prompt ('Have you ever fixed a helicopter before?').toLowerCase();
+        q3 = prompt ('Invalid input. Do you believe that Apache attack helicopter can only fly less than 200 miles without refueling?').toLowerCase();
     }
     if (q3 == 'y' || q3 == 'yes'){
-        a3 = 'Great! Fixing helicopters are fun, right?';
+        a3 = 'Got you! The Apache can fly 296 miles max without refueling!';
         alert (a3);
-    }    else if (q3 == 'no' || q3 == 'n'){
-        a3 = 'No worrys, working on a helicopter feels no different than a lawn mower!';
+    } else if (q3 == 'no' || q3 == 'n'){
+        a3 = 'Correct! The Apache can fly 296 miles max without refueling! ';
         alert (a3);
+        score += 1;
     }
- 
-// this is question 4
 
-    var q4 = prompt ('Have you ever done a wall painting before?').toLowerCase();
+}
+
+function question4(){
+    var q4 = prompt ('Do you think your stomach acid is strong enough to dissolve metal?').toLowerCase();
     var a4 ;
     while (q4 !=='y' && q4 !== 'yes' && q4 !== 'n' && q4!== 'no'){
-        q4 = prompt ('Have you ever done a wall painting before?').toLowerCase();
+        q4 = prompt ('Invalid input. Do you think your stomach acid is strong enough to dissolve metal?').toLowerCase();
     }
     if (q4 == 'y' || q4 == 'yes'){
-        a4 = "Aww, it's not as easy as we thought, right?";
+        a4 = "Aww, that's right! How could know it?";
         alert (a4);
-    }    else if (q4 == 'no' || q4 == 'n'){
-        a4 = "You gotta give a try, it's so interesting";
+        score += 1;
+    } else if (q4 == 'no' || q4 == 'n'){
+        a4 = "Acturarely you can. Try to swallow a piece of metal and you'll find out. LOL ";
         alert (a4);
     }
-  
-// this is question 5
 
-    var q5 = prompt ('Do you have kids?').toLowerCase();
+}
+
+function question5(){
+    var q5 = prompt ('Can you believe that golf has been played on the moon?').toLowerCase();
     var a5 ;
     while (q5 !=='y' && q5 !== 'yes' && q5 !== 'n' && q5!== 'no'){
-        q5 = prompt ('Do you have kids?').toLowerCase();
+        q5 = prompt ('Invalid input. Can you believe that golf has been played on the moon?').toLowerCase();
     }
     if (q5 == 'y' || q5 == 'yes'){
-        a5 = 'Sometimes dealing with kid could be tough, right?';
+        a5 = "That's right! In 1971, Alan Shepard hit two golf balls on the lunar surface with a makeshift 6-iron.";
         alert (a5);
-    }    else if (q5 == 'no' || q5 == 'n'){
-        a5 = 'Enjoy your life as hard as you could! Once you got kid, it will change a lot!';
+        score += 1;
+    } else if (q5 == 'no' || q5 == 'n'){
+        a5 = 'In 1971, Alan Shepard hit two golf balls on the lunar surface with a makeshift 6-iron.';
         alert (a5);
     }
-
-    document.getElementById("answer").innerHTML = a1 + "<br>" + a2 + "<br>" + a3 +"<br>" + a4 + "<br>" + a5 + "<br>";
-
-
 }
 
-
-//the following is the number guessing game:
-function numGuess(){
-    var guessNum = prompt("Let's play a number guessing game. You will have total of 4 chances to guess it. Please input a number.");
+function question6(){
+    var guessNum = prompt("Please guess the magic number.");
     var myNum = 11;
-        for (var i=0; i<4; i++){
-            while ( isNaN(guessNum) ){
-                guessNum = prompt('Invalid Input. Please guess a number');
-            }
-            var realNum = Number(guessNum);
-            if (realNum > myNum && i < 3){
-                alert('Your guess number is too high!');
-                guessNum = prompt ('Please guess another number');
-            } else if(realNum < myNum && i < 3){
-                alert('Your guess number is too low!');
-                guessNum = prompt ('Please guess another number');
-            } else if (realNum === myNum){
-                alert("Unbelievable!! You guessed it!!");
-                document.getElementById("numberGame").innerHTML = 'Unbelievable!! You guessed it!!!' + '<br>';
-                break;
-            } else if (i===3){
-                document.getElementById("numberGame").innerHTML = "Sorry, all 4 chances are used and you still don't have it! LOL!" + '<br>';
-            }
+    for (var i=0; i<4; i++){
+        while ( isNaN(guessNum) || guessNum == '' ){
+            guessNum = prompt('Invalid Input. Please guess the magic number');
         }
+        var realNum = Number(guessNum);
+        if (realNum > myNum && i < 3){
+            alert('Your number is too high!');
+            guessNum = prompt ('Please guess another number');
+        } else if(realNum < myNum && i < 3){
+            alert('Your number is too low!');
+            guessNum = prompt ('Please guess another number');
+        } else if (realNum === myNum){
+            alert("Unbelievable!! You figured it out!!");
+            document.getElementById("numberGame").innerHTML = 'Unbelievable!! You guessed my magic number!!!' + '<br>';
+            score += 1;
+            break;
+        } else if (i===3){
+            document.getElementById("numberGame").innerHTML = "Sorry, all 4 chances are used! The magic number in my head is 11, LOL!" + '<br>';
+        }
+    }
 }
 
+function showMyscore(){
+    document.getElementById("answer").innerHTML = 'Your final score is ' + Math.floor(score/7*100) +"%." + "<br>";
+}
+
+
+
+// function quiz(){
+//     var score =0;
+//     alert('You are about to play the guessing game! The first 5 questions will only take "yes", "y", "no" or "n" as the answer. ');
+//     // this is question 1
+//     var q1 = prompt('Does the US has more land area than China?').toLowerCase();
+//     var a1 ;
+//     while (q1 !=='y' && q1 !== 'yes' && q1 !== 'n' && q1 !== 'no'){
+//         q1 = prompt ('Invalid input. Does U.S. has more land than China? ').toLowerCase();
+//     }
+//     if (q1 == 'y' || q1 == 'yes'){
+//         a1 = 'Acturarely, China is 2.2% larger than the US land area.';
+//         alert (a1);
+//     } else if (q1 == 'no' || q1 == 'n'){
+//         a1 = 'Correct! China is 2.2% larger than the US land area.';
+//         alert (a1);
+//         score += 1;
+//     }
+
+//     // this is the question 2
+
+//     var q2 = prompt ('Will you stop buying corona beer due to the coronavirus?').toLowerCase();
+//     var a2 ;
+//     while (q2 !=='y' && q2 !== 'yes' && q2 !== 'n' && q2 !== 'no'){
+//         q2 = prompt ('Invalid input. Will you stop buying corona beer due to the coronavirus?').toLowerCase();
+//     }
+//     if (q2 == 'y' || q2 == 'yes'){
+//         a2 = 'Well, The beer has nothing to do with the virus! Those two are not related at all!';
+//         alert (a2);
+//     } else if (q2 == 'no' || q2 == 'n'){
+//         a2 = 'Great! The beer has nothing to do with the virus!';
+//         alert (a2);
+//         score += 1;
+//     }
+
+//     // this is question 3
+
+//     var q3 = prompt ('Do you think Apache attach helicopter an only fly 200 miles without refueling?').toLowerCase();
+//     var a3 ;
+//     while (q3 !=='y' && q3 !== 'yes' && q3 !== 'n' && q3 !== 'no'){
+//         q3 = prompt ('Invalid input. Do you believe that Apache attack helicopter can only fly less than 200 miles without refueling?').toLowerCase();
+//     }
+//     if (q3 == 'y' || q3 == 'yes'){
+//         a3 = 'Got you! The Apache can fly 296 miles max without refueling!';
+//         alert (a3);
+//     } else if (q3 == 'no' || q3 == 'n'){
+//         a3 = 'Correct! The Apache can fly 296 miles max without refueling! ';
+//         alert (a3);
+//         score += 1;
+//     }
+
+//     // this is question 4
+
+//     var q4 = prompt ('Do you think your stomach acid is strong enough to dissolve metal?').toLowerCase();
+//     var a4 ;
+//     while (q4 !=='y' && q4 !== 'yes' && q4 !== 'n' && q4!== 'no'){
+//         q4 = prompt ('Invalid input. Do you think your stomach acid is strong enough to dissolve metal?').toLowerCase();
+//     }
+//     if (q4 == 'y' || q4 == 'yes'){
+//         a4 = "Aww, that's right! How could know it?";
+//         alert (a4);
+//         score += 1;
+//     } else if (q4 == 'no' || q4 == 'n'){
+//         a4 = "Acturarely you can. Try to swallow a piece of metal and you'll find out. LOL ";
+//         alert (a4);
+//     }
+
+//     // this is question 5
+
+//     var q5 = prompt ('Can you believe that golf has been played on the moon?').toLowerCase();
+//     var a5 ;
+//     while (q5 !=='y' && q5 !== 'yes' && q5 !== 'n' && q5!== 'no'){
+//         q5 = prompt ('Invalid input. Can you believe that golf has been played on the moon?').toLowerCase();
+//     }
+//     if (q5 == 'y' || q5 == 'yes'){
+//         a5 = "That's right! In 1971, Alan Shepard hit two golf balls on the lunar surface with a makeshift 6-iron.";
+//         alert (a5);
+//         score += 1;
+//     } else if (q5 == 'no' || q5 == 'n'){
+//         a5 = 'In 1971, Alan Shepard hit two golf balls on the lunar surface with a makeshift 6-iron.';
+//         alert (a5);
+//     }
+
+//     //the following is the number guessing game:
+//     var guessNum = prompt("Let's play a number guessing game now . You will have total of 4 chances to guess it. Please input a number.");
+//     var myNum = 11;
+//         for (var i=0; i<4; i++){
+//             while ( isNaN(guessNum) || guessNum == '' ){
+//                 guessNum = prompt('Invalid Input. Please guess a number');
+//             }
+//             var realNum = Number(guessNum);
+//             if (realNum > myNum && i < 3){
+//                 alert('Your guess number is too high!');
+//                 guessNum = prompt ('Please guess another number');
+//             } else if(realNum < myNum && i < 3){
+//                 alert('Your guess number is too low!');
+//                 guessNum = prompt ('Please guess another number');
+//             } else if (realNum === myNum){
+//                 alert("Unbelievable!! You guessed it!!");
+//                 document.getElementById("numberGame").innerHTML = 'Unbelievable!! You guessed my magic number!!!' + '<br>';
+//                 score += 1;
+//                 break;
+//             } else if (i===3){
+//                 document.getElementById("numberGame").innerHTML = "Sorry, all 4 chances are used! The magic number in my head is 11, LOL!" + '<br>';
+//             }
+//         }
+//         // while (mutilCho !== 'done'){
+//         //     var mutilCho=prompt('Polar bears are sometime lonely, hard to find another polar bear to mate with. So, from the following list, what do you think the polar bear have been mating with?  A. a human.   B. another polar bear.  C. a tree hole.  D. grizzlies.  Input "done" when finished.' ).toLowerCase();
+//         //     var yourAns = [mutilCho[i]]
+//         // }
+//         // var myAnswer = ['b','d'];
+//         // for (var a=0; a<6; a++){
+//         //     while
+//         // }
+//     document.getElementById("answer").innerHTML = 'Your final score is ' + Math.floor(score/7*100) +"%." + "<br>";
+// }
